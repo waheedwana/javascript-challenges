092d9f29717226795e7bc0c9d134277ff4945236
Waheedullah <waheedwana123@gmail.com>
2026-04-21 15:08:59 +0430

    Create hasEqualXandO.js

---
+++ hasEqualXandO.js
@@ -0,0 +1,26 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+
+function XO(str) {
+    //code here
+  
+  let find = str.toLowerCase();
+  let x = 0;
+  let o = 0
+  for(let num of find){
+    if(num === "o"){
+      o++
+    } else if (num === "x"){
+      x++
+    }
+  }
+  return x === o;
+}
