31f8d1baf40dd394336de372a079c645effbde38
Waheedullah <waheedwana123@gmail.com>
2026-04-28 23:46:50 +0430

    Create learnSlice.js

---
+++ learnSlice.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// Inputs: "abc", "bc"
+// Output: true
+
+// Inputs: "abc", "d"
+// Output: false
+
+
+function solution(str, ending){
+  return str.slice(str.length - ending.length) === ending;
+}
